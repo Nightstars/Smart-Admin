962fb863c49a98e65de448a8ecb39d80e97c9366
Nightstars <ChristZhangowner@gmail..com>
2022-05-18 17:49:51 +0800

    change CDN

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ const assetsCDN = {
   css: [
   ],
   js: [
-    '//unpkg.com/ajax/libs/vue/2.6.11/vue.min.js',
+    '//unpkg.com/vue@2.6.11/dist/vue.min.js',
     '//unpkg.com/vue-router@3.3.4/dist/vue-router.min.js',
     '//unpkg.com/vuex@3.4.0/dist/vuex.min.js',
     '//unpkg.com/axios@0.19.2/dist/axios.min.js',
